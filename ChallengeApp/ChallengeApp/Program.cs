458cbe8bb8c343deedb1eda836a41346046757aa
stefanj23 <stefanj23@gmail.com>
2023-11-08 09:40:54 +0100

    zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool female = true;
+var age = 33;
+
+
+if (female && age < 30)
+{ 
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33"); 
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni chłop");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// var myAge = 36;
+
+int myAge = 120;
+int = myAge + 5;
+Console.WriteLine(myAge)
+
+
+
+
+
