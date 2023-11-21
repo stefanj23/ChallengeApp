140264b357caf5f05be84405f0121e13f36c1bae
stefanj23 <stefanj23@gmail.com>
2023-11-21 11:04:56 +0100

    class do dnia 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace ChallengeApp
+{     public class Employee
+
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public List<int> score = new List<int>();
+        internal object FullInfo;
+
+                public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
